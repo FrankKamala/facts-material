b832e8f09ccb575dfbab616b10607631889f3922
FrankKamala <frankkamala@gmail.com>
2020-04-01 13:59:57 +0300

    Add Splash screen and add animation list

---
+++ app/src/main/java/com/example/myapplication/Splash.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+    ConstraintLayout myConstraint;
+    AnimationDrawable myAnimator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        myConstraint=(ConstraintLayout)findViewById(R.id.splashYetu);
+        myAnimator=(AnimationDrawable)myConstraint.getBackground();
+        myAnimator.setEnterFadeDuration(2000);
+        myAnimator.setExitFadeDuration(2000);
+        myAnimator.start();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this,LoginActivity.class)); //starts main Activity
+                finish();
+
+            }
+        },  3000);
+    }
+    }
+}
