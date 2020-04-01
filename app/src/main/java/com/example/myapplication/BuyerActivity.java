92c780bc16f2e0dcc29acadd97b27cdf554818ef
FrankKamala <frankkamala@gmail.com>
2020-04-01 13:18:19 +0300

    check auth on buyer implemented

--- app/src/main/java/com/example/myapplication/BuyerActivity.java
+++ app/src/main/java/com/example/myapplication/BuyerActivity.java
@@ -1,8 +1,17 @@
 package com.example.myapplication;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.myapplication.models.User;
+import com.example.myapplication.network.ApiClient;
+import com.example.myapplication.network.FactsAfricaApi;
+import com.example.myapplication.utils.FactsPreferences;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -10,7 +19,12 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class BuyerActivity extends AppCompatActivity {
+    private FactsPreferences factsBuyerPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +39,74 @@ public class BuyerActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 //        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        factsBuyerPreferences = new FactsPreferences(this);
+        authListener();
+    }
+    public void authListener() {
+        FactsAfricaApi service = ApiClient.getClient().create(FactsAfricaApi.class);
+        Call<User> call = service.getLoggedUser();
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if (!response.isSuccessful()) {
+                    showErrorDialog();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                showErrorSnackbar();
+            }
+        });
+    }
+
+
+
+    /* Error handling */
+    private void showErrorDialog() {
+        new MaterialAlertDialogBuilder(this, R.style.ThemeOverlay_MaterialComponents_Dialog_Alert)
+                .setTitle("Login failed!")
+                .setIcon(R.drawable.ic_error_outline_red_24dp)
+                .setCancelable(false)
+                .setMessage("Your session has expired. Please login again.")
+                .setPositiveButton("LOGIN", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        logout();
+                        login();
+                        finish();
+                    }
+                })
+                .setNegativeButton("EXIT", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .show();
+    }
+    /*Error snackbar*/
+    private void showErrorSnackbar() {
+        View rootView = findViewById(android.R.id.content);
+        Snackbar snackbar = Snackbar.make(rootView,
+                "Network error!.", Snackbar.LENGTH_INDEFINITE);
+        snackbar.setActionTextColor(getResources().getColor(R.color.design_default_color_error));
+        snackbar.setAction("Please Retry", v -> {
+            authListener();
+            snackbar.dismiss();
+        });
+        snackbar.show();
+    }
+
+    /* Login */
+    private void login() {
+        Intent loginIntent = new Intent(this, LoginActivity.class);
+        startActivity(loginIntent);
+    }
+
+    public void logout() {
+        factsBuyerPreferences.setLoggedIn(false);
     }
 
 }
--- app/src/main/java/com/example/myapplication/buyer_ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/buyer_ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,11 +12,34 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.example.myapplication.BuyerActivity;
 import com.example.myapplication.R;
+import com.example.myapplication.adapters.InvoiceAdapter;
+import com.example.myapplication.models.Invoice;
+import com.google.android.material.appbar.MaterialToolbar;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HomeFragment extends Fragment implements View.OnClickListener{
 
+    private InvoiceAdapter invoiceAdapter;
+    private List<Invoice> invoices;
+    private View root;
+    private static final int SORT_AMOUNT = 0;
+    private static final int SORT_DATE_CREATED = 1;
+    private static final int SORT_DATE_DUE = 2;
+    private int currentSort = SORT_DATE_CREATED;
+    @BindView(R.id.invoiceRecycler)
+    RecyclerView mInvoiceRecycler;
+    @BindView(R.id.invoiceToolbar)
+    MaterialToolbar mInvoicesToolbar;
+    @BindView(R.id.invoiceSearch) androidx.appcompat.widget.SearchView mInvoiceSearch;
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -24,7 +48,16 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home_buyer, container, false);
 
+        ButterKnife.bind(this, root);
+        ((BuyerActivity) getActivity()).authListener();
+        loadInvoices();
 
+        mInvoicesToolbar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.sort) {
+                onSortClicked();
+            }
+            return false;
+        });
         return root;
     }
 
